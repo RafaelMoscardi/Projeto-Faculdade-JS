9d3ced02b3cfcddd8e86283a235812b4b406dd4c
Rafael Moscardi <Rafaelmoscardi07@gmail.com>
2023-04-06 21:47:48 -0300

    1/2 Aula
    Completas

---
+++ conteudo-basico/js/script.js
@@ -0,0 +1,5 @@
+//alert("Olá Mundo!")
+/** 
+vamos criar uma seleção de Elemento no HTML
+*/
+document.getElementById("exemplo").innerHTML = "NOVO TEXTO"
---
+++ declaracao-variaveis/js/script.js
@@ -0,0 +1,19 @@
+let nome = "João"
+console.log(nome)
+nome = 100
+console.log(nome)
+
+console.log(nomeCompleto)
+var nomeCompleto = "João da Silva"
+
+nomeCompleto = 200
+console.log(nomeCompleto)
+
+const elemento = "algumTexto"
+console.log(elemento)
+
+const dolarParalelo = 5.06
+console.log("DOLAR " + dolarParalelo)
+
+const diaSemana = ["SEG", "TER", "QUA", "QUI", "SEX", "SAB", "DOM"]
+console.log("DIA DA SEMANA " + diaSemana[0])
