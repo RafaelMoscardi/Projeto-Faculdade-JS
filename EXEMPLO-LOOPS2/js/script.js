0a59b993a5125e4380237417727b817599ebc7c7
Rafael Moscardi <99302069+RafaelMoscardi@users.noreply.github.com>
2023-04-29 23:01:00 -0300

    AULA 7

---
+++ EXEMPLO-LOOPS2/js/script.js
@@ -0,0 +1,6 @@
+// Através da função getElementbyId(nomeId)
+const meuElemento = document.getElementById("msg")
+
+function alteraMsg(){
+    meuElemento.innerHTML = "Olá Mundo disparado pela Função!"
+}
